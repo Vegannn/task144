2b44e7ffd92b9d6b34091fece350977761f6e580
Rafal Stefaniak <vegecia@gmail.com>
2018-04-26 13:26:01 +0200

    Design edited

--- components/Contact.js
+++ components/Contact.js
@@ -10,8 +10,8 @@ var Contact = React.createClass({
           className: 'contactImage',
           src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
         }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('p', {className: 'contactLabel name'}, 'Imię: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel2 forname'}, 'Nazwisko: ' + this.props.item.lastName),
         React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
           this.props.item.email
         )
